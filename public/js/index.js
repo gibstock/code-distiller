d76bca9eb9fc35e76a13d850c6f9db78461d330f
Andre Gonzales <gibstock@gmail.com>
2022-06-20 09:55:17 -0700

    Added subscribe form

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,9 @@ const cards = document.querySelectorAll('.card')
 const searchIcon = document.querySelector('.search-icon')
 const searchBox = document.querySelector('.search-box')
 const socialButtons = document.querySelectorAll('.follow-circle')
+const subscibeBtn = document.querySelector('.subscribe')
+const subscribeForm = document.getElementById('subscribe-form')
+const closeBtn = document.querySelector('.close-button')
 
 cards.forEach((card) => {
   card.addEventListener('click', (e) => {
@@ -26,4 +29,19 @@ socialButtons.forEach((button) => {
   button.addEventListener('click', ()=> {
     window.open(button.dataset.uri, '_blank')
   })
+})
+
+subscibeBtn.addEventListener('click', () => {
+  subscribeForm.style.display = 'flex'
+})
+
+closeBtn.addEventListener('click', () => {
+  subscribeForm.style.display = 'none'
+})
+subscibeBtn.addEventListener('touchstart', () => {
+  subscribeForm.style.display = 'flex'
+})
+
+closeBtn.addEventListener('touchstart', () => {
+  subscribeForm.style.display = 'none'
 })
